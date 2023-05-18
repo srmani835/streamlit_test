2b9b98ed7d2f3ff9335ea469162c384574db1883
S.MANIKANTESWARA REDDY <96164403+srmani835@users.noreply.github.com>
2023-05-18 21:34:46 +0530

    Update main.py
    Added skiprows condition

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 
 
 def readcsv(csv):
-    df = pd.read_csv(csv,encoding= 'unicode_escape',on_bad_lines='skip')
+    df = pd.read_csv(csv,encoding= 'unicode_escape',on_bad_lines='skip',skiprows=2)
     return df
 
 def convert_df(df):
